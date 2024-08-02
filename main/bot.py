200718579496dd146790b028dffa303a169296af
Pavel Kulebyakin <pavel.kulebyakin.study@gmail.com>
2024-08-02 23:50:31 +0300

    Added pair conversion functionality + menu
    Added bot commands menu

--- main/bot.py
+++ main/bot.py
@@ -1,7 +1,7 @@
 import logging
 import requests
 
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, BotCommand
 from telegram.ext import CommandHandler, Application, ContextTypes, CallbackQueryHandler
 from config import TELEGRAM_TOKEN, EXCHANGE_RATE_API_KEY
 
@@ -10,7 +10,7 @@ ITEMS_PER_PAGE = 20
 # Настройка логирования
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-# logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
@@ -79,14 +79,77 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     await supported_currencies_handler(update, context, page)
 
 
+async def fetch_pair_conversion(base_currency: str, target_currency: str, amount: str):
+    url = f'https://v6.exchangerate-api.com/v6/{EXCHANGE_RATE_API_KEY}/pair/{base_currency}/{target_currency}/{amount}'
+    with requests.get(url) as response:
+        return response
+
+
+async def pair_conversion_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    if not context.args or len(context.args) < 2:
+        await update.message.reply_text('Пожалуйста, укажите базовую и целевую валюты. Пример: /rate RUB USD')
+        return
+
+    base_currency = context.args[0]
+    target_currency = context.args[1]
+
+    if not (len(base_currency) == 3 and len(target_currency) == 3
+            and base_currency.isalpha() and target_currency.isalpha()):
+        await update.message.reply_text('Код валюты должен состоять из трех латинских букв. Пример: RUB')
+        return
+
+    amount = 1
+
+    if len(context.args) == 3 and not context.args[2].isdigit():
+        await update.message.reply_text('Количество валюты должно быть записано в виде цифры.\n'
+                                        'Пример: /rate RUB USD 100.')
+        return
+
+    if len(context.args) == 3 and context.args[2].isdigit():
+        amount = context.args[2]
+
+    api_response = await fetch_pair_conversion(base_currency, target_currency, amount)
+
+    if not api_response.ok:
+        await update.message.reply_text('Ошибка при получении данных. '
+                                        'Пожалуйста, проверьте данные или попробуйте позже.')
+        return
+
+    data = api_response.json()
+
+    if data['result'] == 'error':
+        await update.message.reply_text(f'Ошибка: {data["error-type"]}')
+        return
+
+    conversion_result = data['conversion_result']
+    reply_message = f"Курс обмена : {amount} {base_currency} = {conversion_result} {target_currency}"
+
+    await update.message.reply_text(reply_message)
+
+
+async def post_init(application: Application) -> None:
+    bot_commands = [
+        BotCommand("rate", "Получить курс валют"),
+        BotCommand("list", "Список поддерживаемых валют"),
+        BotCommand("help", "Помощь")
+    ]
+    await application.bot.set_my_commands(bot_commands)
+
+
 def main() -> None:
 
-    application = Application.builder().token(TELEGRAM_TOKEN).build()
+    application = Application.builder().token(TELEGRAM_TOKEN).post_init(post_init).build()
 
-    application.add_handler(CommandHandler("start", start_handler))
-    application.add_handler(CommandHandler("help", help_handler))
-    application.add_handler(CommandHandler("supported", supported_currencies_handler))
-    application.add_handler(CallbackQueryHandler(button_handler))
+    handlers = [
+        CommandHandler("start", start_handler),
+        CommandHandler("help", help_handler),
+        CommandHandler("list", supported_currencies_handler),
+        CommandHandler("rate", pair_conversion_handler),
+        CallbackQueryHandler(button_handler)
+    ]
+
+    application.add_handlers(handlers)
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
